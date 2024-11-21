4fe2201537d29340f480129a9fe2d8e21db3de2b
Hasankhan170 <hasankhanali0316@gmail.com>
2024-11-21 16:16:31 +0500

    some issues

--- index.js
+++ index.js
@@ -18,3 +18,21 @@ connectDB()
 .catch((err)=>{
     console.log("MONGO DB connection failed !!! ", err);
 })
+
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//   try {
+//     const connectionInstance = await mongoose.connect(
+//       `${process.env.MONGO_URI}practice`
+//     );
+//     console.log(
+//       `MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
+//     );
+//   } catch (error) {
+//     console.log("MONGODB connection FAILED ", error);
+//     process.exit(1);
+//   }
+// };
+
+// export default connectDB;
