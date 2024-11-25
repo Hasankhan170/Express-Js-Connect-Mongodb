6e5fe0f13814f245d13cd45ca690a2860c967188
Hasankhan170 <hasankhanali0316@gmail.com>
2024-11-25 21:24:44 +0500

    todo added done

--- src/controllers/todos.control.js
+++ src/controllers/todos.control.js
@@ -1,6 +1,24 @@
-
+import Todos from "../models/todos.model.js"
 
 
 const addTodo = (req,res)=>{
     const {title,description} = req.body;
+
+    if(!title || !description){
+        res.status(400).json({
+            massage : "title and description are required"
+        })
+        return
+    }
+
+    const todo = Todos.create({
+        title,
+        description
+    })
+    res.status(201).json({
+        massage : "todo added successfully",
+        data : todo
+    })
 }
+
+export {addTodo}
